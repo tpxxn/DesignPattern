438b26b98ccc36e94ad5dbf50c6de742fe329ee3
tpxxn <351765204@qq.com>
2021-02-19 09:46:56 +0800

    [refactor]调用模式方法

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -8,30 +8,38 @@ namespace DesignPattern
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("");
-            /*====工厂方法模式====*/
-            Console.WriteLine("====工厂方法模式====");
+            CreationalPattern();
+        }
+
+        private static void CreationalPattern()
+        {
+            CallPattern("工厂方法模式", () =>
+            {
+                new FactoryMethod.Client().Main();
+            });
+            CallPattern("抽象工厂模式", () =>
+            {
+                new AbstractFactory.Client().Main();
+            });
+            CallPattern("生成器模式", () =>
+            {
+                new Builder.Program().Main();
+            });
+            CallPattern("原型模式", () =>
+            {
+                new Prototype.Program().Main();
+            });
+            CallPattern("单例模式", () =>
+            {
+                new Singleton.Program().Main();
+            });
+        }
+
+        private static void CallPattern(string patternChineseName, Action callback)
+        {
+            Console.WriteLine($"===={patternChineseName}====");
             Console.WriteLine("");
-            new FactoryMethod.Client().Main();
-            Console.WriteLine("");
-            /*====抽象工厂模式====*/
-            Console.WriteLine("====抽象工厂模式====");
-            Console.WriteLine("");
-            new AbstractFactory.Client().Main();
-            Console.WriteLine("");
-            /*====生成器模式====*/
-            Console.WriteLine("====生成器模式====");
-            Console.WriteLine("");
-            new Builder.Program().Main();
-            Console.WriteLine("");
-            /*====原型模式====*/
-            Console.WriteLine("====原型模式====");
-            Console.WriteLine("");
-            new Prototype.Program().Main();
-            Console.WriteLine("");
-            /*====单例模式====*/
-            Console.WriteLine("====单例模式====");
-            Console.WriteLine("");
-            new Singleton.Program().Main();
+            callback();
             Console.WriteLine("");
         }
     }
